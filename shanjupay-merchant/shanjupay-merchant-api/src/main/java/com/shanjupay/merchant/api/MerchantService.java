0c1c24318147409ea0281d5844fd0d2acf00da69
HSY <1224143192@qq.com>
2021-01-04 20:02:59 +0800

    环境搭建—添加Swagger测试工具

---
+++ shanjupay-merchant/shanjupay-merchant-api/src/main/java/com/shanjupay/merchant/api/MerchantService.java
@@ -0,0 +1,12 @@
+package com.shanjupay.merchant.api;
+
+import com.shanjupay.merchant.api.dto.MerchantDTO;
+
+public interface MerchantService {
+    /**
+     * 根据ID查询详细信息
+     * @param merchantId
+     */
+    MerchantDTO queryMerchantById(Long merchantId);
+
+}
---
+++ shanjupay-merchant/shanjupay-merchant-service/src/main/java/com/shanjupay/merchant/service/MerchantServiceImpl.java
@@ -0,0 +1,30 @@
+package com.shanjupay.merchant.service;
+
+import com.shanjupay.merchant.api.MerchantService;
+import com.shanjupay.merchant.api.dto.MerchantDTO;
+import com.shanjupay.merchant.entity.Merchant;
+import com.shanjupay.merchant.mapper.MerchantMapper;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class MerchantServiceImpl implements MerchantService {
+
+    @Autowired
+    MerchantMapper merchantMapper;
+
+    /**
+     * 根据ID查询详细信息
+     * @param merchantId
+     * @return
+     */
+    @Override
+    public MerchantDTO queryMerchantById(Long merchantId) {
+        Merchant merchant = merchantMapper.selectById(merchantId);
+        MerchantDTO merchantDTO = new MerchantDTO();
+        merchantDTO.setId(merchant.getId());
+        merchantDTO.setMerchantName(merchant.getMerchantName());
+        //设置其它属性...
+        return merchantDTO;
+    }
+}
