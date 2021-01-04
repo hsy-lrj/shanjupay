0c674c91098f6c4d312f71c49d4bd50ff135381f
HSY <1224143192@qq.com>
2021-01-04 19:59:33 +0800

    环境搭建—添加Swagger测试工具

--- shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/MerchantApplicationBootstrap.java
+++ shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/MerchantApplicationBootstrap.java
@@ -3,9 +3,12 @@ package com.shanjupay.merchant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class MerchantApplicationBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(MerchantApplicationBootstrap.class, args);
